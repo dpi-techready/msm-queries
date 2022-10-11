6f880d83817234c8ac16640a5040cf3990a32809
rixiobarrios <rixiobarrios.consulado@gmail.com>
2022-10-11 19:55:31 +0000

    Updated project

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
+# Dynamics routes have an importance in order, please note
+
 Rails.application.routes.draw do
+get("/directors/:an_id", { :controller => "directors", :action => "director_details"})  
+get("/directors/eldest", {:controller => "directors", :action => "wisest"})
+get("/directors/youngest", {:controller => "directors", :action => "rookie"})
+
   get("/", { :controller => "application", :action => "homepage" })
 
   get("/directors", {:controller => "directors", :action => "index" })
