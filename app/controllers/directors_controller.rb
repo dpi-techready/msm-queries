ed0ac05ddfd4f8b25583e62fd2636c9917c6bea0
rixiobarrios <rixiobarrios.consulado@gmail.com>
2022-10-12 03:25:14 +0000

    Passed 92% of rails grade test

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -13,9 +13,9 @@ class DirectorsController < ApplicationController
     render({ :template => "director_templates/youngest.html.erb"})
   end  
   def director_details
-    # params looks like this {"an_id => "42"}
     the_id = params.fetch("an_id")
     @the_director = Director.where({ :id => the_id }).at(0)
+    @filmography = Movie.where({ :director_id => @the_director.id })
     render({ :template => "director_templates/show.html.erb"})
   end  
 end    
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 # Dynamics routes have an importance in order, please note
 
 Rails.application.routes.draw do
-get("/directors/:an_id", { :controller => "directors", :action => "director_details"})  
-get("/directors/eldest", {:controller => "directors", :action => "wisest"})
-get("/directors/youngest", {:controller => "directors", :action => "rookie"})
+  get("/directors/eldest", {:controller => "directors", :action => "wisest"})
+  get("/directors/youngest", {:controller => "directors", :action => "rookie"})
 
+  get("/directors/:an_id", { :controller => "directors", :action => "director_details"})  
+  get("/movies/:an_id", { :controller => "movies", :action => "movie_details"})
+  get("/actors/:an_id", {:controller => "actors", :action => "actor_details"})
+  
   get("/", { :controller => "application", :action => "homepage" })
-
+  
   get("/directors", {:controller => "directors", :action => "index" })
+  get("/movies", {:controller => "movies", :action => "index"})
+  get("/actors", {:controller => "actors", :action => "index"})
 end
